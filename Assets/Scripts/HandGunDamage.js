6f1eddd70573d795f92802bee2c5a18d55dcfc2f
mapineda <mapineda1222@gmail.com>
2016-04-26 17:40:13 -0500

    edited handgundamage script

--- Assets/Scripts/HandGunDamage.js
+++ Assets/Scripts/HandGunDamage.js
@@ -9,7 +9,7 @@ var TargetDistance : float;
 var AllowedRange : float = 15;
 
 function Update() {
-	if (input.GetButtonDown("Fire1")) {
+	if (Input.GetButtonDown("Fire1")) {
 		var Shot : RaycastHit;
 			if (Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), Shot)) {
 				TargetDistance = Shot.distance;

3a72540bb868d8f69b0403512141126eea3429c2
mapineda <mapineda1222@gmail.com>
2016-04-26 16:18:41 -0500

    init commit

---
+++ Assets/Scripts/GunFire.js
@@ -0,0 +1,12 @@
+﻿
+// every time a script is called it calls the update function
+function Update() {
+//if mouse button is down then fire gun sound through var gunsound
+//define var gunsound and play gunsound.
+// At the same time, play animation for gun
+	if (Input.GetButtonDown("Fire1")) {
+		var gunsound : AudioSource = GetComponent.<AudioSource>();
+		gunsound.Play();
+		GetComponent.<Animation>().Play("gunShot");
+	}
+}
---
+++ Assets/Scripts/HandGunDamage.js
@@ -0,0 +1,22 @@
+﻿//script allows you to inflict damage via the gun/weapon
+
+//place the script inside GUNMECHANICS folder
+
+//begin by delcaring variables
+
+var DamageAmount : int = 5;
+var TargetDistance : float;
+var AllowedRange : float = 15;
+
+function Update() {
+	if (input.GetButtonDown("Fire1")) {
+		var Shot : RaycastHit;
+			if (Physics.Raycast (transform.position, transform.TransformDirection(Vector3.forward), Shot)) {
+				TargetDistance = Shot.distance;
+					if (TargetDistance < AllowedRange) {
+						Shot.transform.SendMessage("DeductPoints", DamageAmount);
+					}
+			}
+	}
+} 
+// PLACE SCRIPT ON YOUR ENEMY

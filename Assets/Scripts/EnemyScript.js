5b589d03f12abdf46aace7de55c91a485128c211
mapineda <mapineda1222@gmail.com>
2016-04-26 18:02:24 -0500

    created enemy and enemyscript that destroys enemy upon depeleting enemyhealth with two shots from handgundamage script

---
+++ Assets/Scripts/EnemyScript.js
@@ -0,0 +1,14 @@
+﻿//this script is for our enemy object
+// declare enemy variable health
+
+var EnemyHealth : int = 10;
+// every time the enemy is hit or damaged deduct health points
+function DeductPoints (DamageAmount : int) {
+	EnemyHealth -= DamageAmount;
+}
+//once enemyhealth -=0, destroy the gameobject
+function Update () {
+	if (EnemyHealth <= 0) {
+		Destroy(gameObject);
+	}
+}
